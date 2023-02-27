c1ab2471c670773b68526efea14e85971cadf7f7
rodrigomsrocha <rodrigomtc82@gmail.com>
2023-02-27 16:19:52 -0300

    tabs functionality

--- src/components/EducationSetion/TabLabel/index.tsx
+++ src/components/EducationSetion/TabLabel/index.tsx
@@ -1,11 +1,13 @@
 interface TabProps {
   label: string;
   active: boolean;
+  onActiveTabChange: (tab: string) => void;
 }
 
-export function Tab({ label, active }: TabProps) {
+export function TabLabel({ label, active, onActiveTabChange }: TabProps) {
   return (
     <li
+      onClick={() => onActiveTabChange(label)}
       className={`px-4 py-2 border-l-2 ${
         active && 'border-purple-500 text-purple-500'
       } hover:bg-violet-200 hover:text-purple-500 transition cursor-pointer`}
--- src/components/EducationSetion/index.tsx
+++ src/components/EducationSetion/index.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import { Title } from '../Title';
-import { Tab } from './Tab';
+import { TabLabel } from './TabLabel';
 
 const tabs = ['Rocketseat', 'Origamid', 'Cod3r', 'CCAA'];
 
 export function EducationSection() {
   const [activeTab, setActiveTab] = useState('Rocketseat');
+
+  const handleActiveTabChange = (tab: string) => {
+    setActiveTab(tab);
+  };
 
   return (
     <div id="education" className="min-h-screen flex flex-col justify-center">
@@ -13,11 +17,20 @@ export function EducationSection() {
       <div className="flex gap-4">
         <ul>
           {tabs.map((tab) => {
-            return <Tab key={tab} label={tab} active={tab === activeTab} />;
+            return (
+              <TabLabel
+                key={tab}
+                label={tab}
+                active={tab === activeTab}
+                onActiveTabChange={() => {
+                  handleActiveTabChange(tab);
+                }}
+              />
+            );
           })}
         </ul>
         <div className="p-2">
-          <h3>React @Rocketseat</h3>
+          <h3>React @{activeTab}</h3>
         </div>
       </div>
     </div>
